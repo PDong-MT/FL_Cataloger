17e9b5059c1945cf572054403028264b39e19d3d
Patrick MacDonald <66454366+PDong-MT@users.noreply.github.com>
2022-07-22 09:32:20 -0500

    7-22 am
    removing flotsam

--- public/blankity.js
+++ public/blankity.js
@@ -10,10 +10,25 @@ const fs = require('fs')
 //const { socket } = require('../src/components/socket');
 
 // Ports
-const socketPort = 42003
-let lynxOutputPort = 44999 || process.env.PORT
-let lynxOutputHost = '127.0.0.1'
-let lynxStore
+// const socketPort = 42003
+// let lynxOutputPort = 44999 || process.env.PORT
+// let lynxOutputHost = '127.0.0.1'
+// let lynxStore
+
+// let placeVal = 0
+let fullDatagram=  ''
+
+
+
+const remoteControl = net.createServer(conn => {
+  let placeHolder = 0
+  console.log('remoteControl is connected') 
+  placeVal = fullDatagram.substring(0,4);
+
+// next line gets info about the camera image
+// conn.write('command=ImageGetInfo;Window=2\r')
+// next line gets the image
+// conn.write(`command=ImageExport;Window=2;Area=-100%,-100%,100%,100%;File=bazooka;Time=22.45\r`)
 
 const resServer = net.createServer(resConn => {
   let placeHolder = 0
@@ -23,17 +38,35 @@ const resServer = net.createServer(resConn => {
     var datagram = Buffer.from(data).toString()
     // 4 characters for place, 24 characters for athlete name, 30 for affiliation
     let placeVal = datagram.substring(0,4);
-    let athName = datagram.substring(4,28);
-    let athAffiliation = datagram.substring(28,58)
-    let time = datagram.substring(58,68);
+
 
     // this is working
     if (placeVal > placeHolder) {
-      getImage(datagram)
+      let athName = datagram.substring(4,28).trimEnd().replace(/ /g, "");
+      let athAffiliation = datagram.substring(28,58).trimEnd().replace(/ /g, "")
+      let time = datagram.substring(58,68).trimStart().trimEnd()
+
+    
+      let imageFileName = `${athAffiliation}_${athName}.jpg`
+
+
+   //   conn.write (`command=ImageDraw;Window=0;Zoom=50%;HashTime=${time};\r`)
+// console.log(`command=ImageExport;Window=2;Time=${time};File=${imageFileName};Area=-100%,-100%,100%,100%\r`)
+     conn.write(`command=ImageExport;Window=2;Time=${time};File=${imageFileName};Area=-100%,-100%,100%,100%\r`)
+
+    // following line is working
+    //  conn.write(`command=ImageExport;Window=2;Time=22:34.83;File=${imageFileName};Area=-100%,-100%,100%,100%\r`)
+
+
+
       placeHolder = placeVal
     } else {
       // nothing, program has run
+      console.log('I have worked through the results')
     }
+
+    
+
   })
 
   resConn.on('end', () => {
@@ -43,13 +76,11 @@ const resServer = net.createServer(resConn => {
 
 resServer.listen(42200)
 
-const remoteControl = net.createServer(conn => {
-  console.log('remoteControl is connected') 
 
-// next line gets info about the camera image
-// conn.write('command=ImageGetInfo;Window=2\r')
-// next line gets the image
-// conn.write(`command=ImageExport;Window=2;Area=-100%,-100%,100%,100%;File=bazooka;Time=22.45\r`)
+
+
+
+
 
   conn.on('end', () => {
     console.log('client disconnected')})
@@ -58,18 +89,10 @@ const remoteControl = net.createServer(conn => {
 
 remoteControl.listen(42000)
 
+
 // const lynxServer = new net.Server()
 
-function getImage(datagram) {
-  console.log('getImage')
-  let athName = datagram.substring(4,28).trimEnd().replace(/ /g, "");
-  let athAffiliation = datagram.substring(28,58).trimEnd().replace(/ /g, "")
-  let time = datagram.substring(58,68).trimEnd().replace(/ /g, "")
 
-   let imageFileName = `${athAffiliation}_${athName}.jpg`
-    console.log((`command=ImageExport;Window=2;Area=-100%,-100%,100%,100%;File=${imageFileName};Time=${time}\r`))
-    // conn.write(`command=ImageExport;Window=2;Area=-100%,-100%,100%,100%;File=${imageFileName};Time=${time}\r`)
-  }
 
 
 // var onevent = client.onevent;
